b76f10bf288214053fbd886a1a2a171a428331e1
Ruben <rportova.work@gmail.com>
2021-05-25 22:09:09 -0500

    CAMBIOS

--- src/main/java/com/peruvianfarma/appweb/repository/ContactoRepository.java
+++ src/main/java/com/peruvianfarma/appweb/repository/ContactoRepository.java
@@ -5,8 +5,8 @@ import com.peruvianfarma.appweb.model.Contacto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface ContactoRepository  extends JpaRepository<Contacto, Integer>{
+public interface  ContactoRepository extends JpaRepository<Contacto, Integer>{
+
     
 }
